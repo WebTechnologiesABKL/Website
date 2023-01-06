b1c4673b50a431612db292c7d0fc7e4a5cc7f4f3
Patrick Langkau <patrick@langkau.info>
2023-01-06 17:14:29 +0100

    Basic WebInterface

---
+++ script.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', function () {
+    const socket = io("ws://localhost:8080", {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"]
+        }
+    });
+
+    function send(){
+        let input = document.getElementById("txfInput").value;
+        document.getElementById("txaChatBox").append("[You]: " + input + "\n");
+        socket.emit('chat', {
+            'message': input
+        });
+        document.getElementById("txfInput").value = "";
+    }
+
+
+    socket.on('welcome', function(data){
+        document.getElementById("txaChatBox").append('[Bot]: You are connected, your ID is "' + data.socketId + '"\n');
+    });
+
+    socket.on('chat', function(data){
+        document.getElementById("txaChatBox").append("[Bot]: " + data.message + "\n");
+    });
+
+    document.getElementById("btnSend").addEventListener("click", send);
+
+});
+
