63ea908d03d69fdd74ae22aa4a2123e0311cc7b5
Patrick Langkau <patrick@langkau.info>
2023-01-07 13:50:42 +0100

    update design

--- script.js
+++ script.js
@@ -8,21 +8,50 @@ window.addEventListener('load', function () {
 
     function send(){
         let input = document.getElementById("txfInput").value;
-        document.getElementById("txaChatBox").append("[You]: " + input + "\n");
-        socket.emit('chat', {
-            'message': input
-        });
-        document.getElementById("txfInput").value = "";
+        if(input){
+            let ul = document.getElementById("chat");
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode(input));
+            li.setAttribute("class", "user");
+            ul.appendChild(li);
+            socket.emit('chat', {
+                'message': input
+            });
+            document.getElementById("txfInput").value = "";
+        }
     }
 
 
     socket.on('welcome', function(data){
-        document.getElementById("txaChatBox").append('[Bot]: You are connected, your ID is "' + data.socketId + '"\n');
+        let ul = document.getElementById("chat");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(data.message));
+        li.setAttribute("class", "bot");
+        ul.appendChild(li);
     });
 
     socket.on('chat', function(data){
-        document.getElementById("txaChatBox").append("[Bot]: " + data.message + "\n");
+        let ul = document.getElementById("chat");
+        let li = document.createElement("li");
+        li.appendChild(document.createTextNode(data.message));
+        li.setAttribute("class", "bot");
+        ul.appendChild(li);
     });
+
+    socket.on('writing', function (data) {
+        if(data.active){
+            let ul = document.getElementById("chat");
+            let li = document.createElement("li");
+            li.appendChild(document.createTextNode("..."));
+            li.setAttribute("class", "bot");
+            li.setAttribute("id", "writing");
+            ul.appendChild(li);
+        }else{
+            let writing = document.getElementById('writing');
+            let chat = document.getElementById('chat');
+            chat.removeChild(writing);
+        }
+    })
 
     document.getElementById("btnSend").addEventListener("click", send);
 
