3767caa5514dd0ca0fc51df9d52474710ce692a0
Patrick Langkau <patrick@langkau.info>
2023-01-24 21:56:12 +0100

    add background video

--- components/Chat.js
+++ components/Chat.js
@@ -1,24 +1,30 @@
-import React from 'react';
+import React, {useRef} from 'react';
 import { useState } from 'react';
 import Head from 'next/head';
 import { createGlobalStyle } from 'tailwind-styled-components';
 import Script from "next/script";
 import Image from "next/image";
-import Minecraft from '/public/minecraft.jpg'
 import { useEffect } from 'react'
 import io from 'socket.io-client'
 
 let socket
 
+let videoName = "Clear_day";
+
 const GlobalStyle = 'createGlobalStyle'
 
 export default function Chatbot() {
+    const videoRef = useRef();
     const [input, setInput] = useState('');
     const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         socketInitializer(), []
     })
+
+    useEffect(() => {
+        videoRef.current?.load();
+    }, [videoName]);
 
     const socketInitializer = async () => {
         if(!socket || !socket.connected){
@@ -29,23 +35,100 @@ export default function Chatbot() {
                 console.log('connected')
             })
             socket.on("welcome", function(data){
-                alert(data.message)
+                let message = data.message
+                setMessages([...messages, { text: message, isUser: false }]);
             })
 
             socket.on("chat", function(data){
-                alert(data.message)
+                let message = data.message
+                setMessages([...messages, { text: message, isUser: false }]);
                 if(data.weather){
-                    console.log(data.weather)
+                    let weather = data.weather
+                    let time = new Date(data.time)
+                    changeVideo(weather, time)
+                    console.log(weather)
                 }
             })
 
             socket.on("writing", function(data){
                 if(data.active){
-                    alert("writing on")
+                    setMessages([...messages, { text: "...", isUser: false }])
                 }else{
-                    alert("writing off")
+                    setMessages(messages.slice(0, -1))
                 }
             })
+        }
+
+        function changeVideo(weather, time){
+            let icon = weather.weather.weather[0].icon
+            let isDay = true
+            if(time.getHours() < 5 || time.getHours() > 18){
+                isDay = false
+            }
+            switch(icon){
+                case "clear-day":
+                    videoName = "Clear_day"
+                    break;
+                case "partly-cloudy-day":
+                    videoName = "Clear_day"
+                    break;
+                case "partly-cloudy-night":
+                    videoName = "Clear_night"
+                    break;
+                case "clear-night":
+                    videoName = "Clear_night"
+                    break;
+
+                case "cloudy":
+                    if(isDay){
+                        videoName = "Clear_day"
+                    }else{
+                        videoName = "Clear_night"
+                    }
+                    break;
+                case "sunny":
+                    if(isDay){
+                        videoName = "Clear_day"
+                    }else{
+                        videoName = "Clear_night"
+                    }
+                    break;
+                case "wind":
+                    if(isDay){
+                        videoName = "Clear_day"
+                    }else{
+                        videoName = "Clear_night"
+                    }
+                    break;
+                case "fog":
+                    if(isDay){
+                        videoName = "Rain_day"
+                    }else{
+                        videoName = "Rain_night"
+                    }
+                    break;
+                case "rain":
+                    if(isDay){
+                        videoName = "Rain_day"
+                    }else{
+                        videoName = "Rain_night"
+                    }
+                    break;
+                case "snow":
+                    if(isDay){
+                        videoName = "Snow_day"
+                    }else{
+                        videoName = "Snow_night"
+                    }
+                    break;
+                case "thunderstorm":
+                    if(isDay){
+                        videoName = "Thunder_day"
+                    }else{
+                        videoName = "Thunder_night"
+                    }
+                    break;
+            }
         }
 
     }
@@ -60,18 +143,20 @@ export default function Chatbot() {
     };
 
     return (
-        <div className="h-screen flex flex-col bg-cover bg-no-repeat bg-fixed bg-center"
-        style={{backgroundImage: "url('/minecraft.jpg')"}}>
+        <div className="h-screen flex flex-col bg-cover bg-no-repeat bg-fixed bg-center">
+            <video autoPlay muted loop className="video" ref={videoRef}>
+                <source src={"/" + videoName + ".mp4"} type="video/mp4"/>
+            </video>
             <div className="h-full flex items-center justify-center">
             <div className="h-4/5 w-2/3 flex-col flex justify-center  bg-blue-500/75">
             <header className="bg-white p-4 flex-shrink-0">
-                <h1 className="text-xl font-medium">Chatbot</h1>
+                <h1 className="text-xl font-medium text-black">Chatbot</h1>
             </header>
             <main className="flex-1 overflow-y-scroll p-4">
                 {messages.map((message, index) => (
                     <div
                         key={index}
-                        className={`bg-white p-2 rounded-lg mb-4 flex-shrink-0 ${
+                        className={`bg-white p-2 rounded-lg mb-4 flex-shrink-0 text-black ${
                             message.isUser ? 'ml-auto' : 'mr-auto'
                         }`}
                     >
@@ -85,11 +170,11 @@ export default function Chatbot() {
                         type="text"
                         value={input}
                         onChange={(e) => setInput(e.target.value)}
-                        className="bg-gray-200 p-2 rounded-lg w-full"
-                        placeholder="Type a message..."
+                        className="bg-gray-200 p-2 rounded-lg w-full text-black"
+                        placeholder="Schreibe eine Nachricht..."
                     />
                     <button type="submit" className="ml-2 p-2 rounded-lg bg-blue-500 text-white">
-                        Send
+                        Senden
                     </button>
                 </form>
             </footer>
