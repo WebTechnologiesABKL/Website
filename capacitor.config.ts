8bcdd1fbe0514c6c96d166827edaeea55b0d86d6
Gr4ph1xZ <patrick@langkau.info>
2023-02-03 16:43:47 +0100

    Create android app (#7)
    * add android build
    
    * add android build
    
    * add android build
    
    * add android build
    
    * add android build
    
    * add android build
    
    * add android build
    
    * add android build
    
    * add android build
    
    * add android build

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.webtechnologiesabkl.wetter',
+  appName: 'Wetter Chatbot',
+  webDir: 'out',
+  bundledWebRuntime: false
+};
+
+export default config;
